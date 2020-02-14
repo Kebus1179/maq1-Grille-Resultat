ae3dac01d2c2036141f942285f583decd6e1f82e
Kebus1179 <61005347+Kebus1179@users.noreply.github.com>
2020-02-14 15:00:20 +0100

    Update demoItem.js

--- src/demoItem.js
+++ src/demoItem.js
@@ -4,7 +4,6 @@ import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { render } from "react-dom";
 
 class DemoItem extends Component {
   constructor(props) {
@@ -20,47 +19,63 @@ class DemoItem extends Component {
     event: React.ChangeEvent<{}>,
     isExpanded: boolean
   ) => {
-    console.log("-----");
-    const valueExpanded = this.state.expanded;
-    if (this.state.expanded === panel.item) {
+    //    console.log("-----");
+    //    console.log("PANEL: ", panel);
+    if (this.state.expanded === panel) {
       this.setState({
         expanded: false
       });
     } else {
       this.setState({
-        expanded: panel.item
+        expanded: panel
       });
     }
   };
 
   createTasks(item, i) {
-    valExp = false;
-    if (this.state.expanded === item) {
+    //   console.log("ITEM", item);
+    let valExp = false;
+    if (this.state.expanded === item.id) {
       valExp = true;
     } else {
       valExp = false;
     }
+    //    console.log("Valeur injecte :", valExp);
     return (
       <ExpansionPanel
         key={i}
         expanded={valExp}
-        onChange={this.handleChange({ item })}
+        onChange={this.handleChange(item.id)}
       >
         <ExpansionPanelSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1bh-content"
           id={item}
         >
-          <Typography>General settings</Typography>
+          <Typography>
+            {item.HourStart} - {item.HourStop}
+          </Typography>
+          <Typography>{item.Company}</Typography>
+          <Typography align="right">
+            {item.Price} {item.Currency} {item.TypeFlight}
+          </Typography>
+          <Typography variant="subtitle1">
+            {" "}
+            {item.CityStart}-{item.CityStop}
+            {item.DureeFlight}
+          </Typography>
+          <Typography>{item.NbEscales}</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Typography>Details panel {item}</Typography>
+          <Typography>Details panel {item.id}</Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
     );
   }
 
   render() {
+    console.log("-----------------");
+    //   console.log("Valeur expanded: ", this.state.expanded);
     var todoEntries = this.props.entries;
     var listItems = todoEntries.map((p, i) => this.createTasks(p, i));
 
