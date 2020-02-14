7a250c471119c859d9e123761cb0fcf2de5d2118
Kebus1179 <61005347+Kebus1179@users.noreply.github.com>
2020-02-14 14:09:31 +0100

    Update demo.js

--- src/demo.js
+++ src/demo.js
@@ -1,39 +1,13 @@
 import React, { Component } from "react";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { render } from "react-dom";
 import DemoItem from "./demoItem";
 
 class Demo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      expanded: false,
-      listItem: ["item1", "item2"]
+      listItem: ["item1", "item2", "item3", "item4", "item5"]
     };
-    this.handleChange = this.handleChange.bind(this);
-    //    this.addItem = this.addItem.bind(this);
   }
-
-  handleChange = (panel: string) => (
-    event: React.ChangeEvent<{}>,
-    isExpanded: boolean
-  ) => {
-    //    setExpanded(isExpanded ? panel : false);
-    console.log("onChange: " + panel);
-    if (this.state.expanded === panel) {
-      this.setState({
-        expanded: false
-      });
-    } else {
-      this.setState({
-        expanded: panel
-      });
-    }
-  };
 
   render() {
     return (
